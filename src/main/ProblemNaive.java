59255189f55d339da141af887e305647b6d17ea0
unknown <Phoenix-Left@hotmail.fr>
2015-03-28 11:49:27 +0100

    calcul score 1 baloon + column distance

--- src/main/ProblemNaive.java
+++ src/main/ProblemNaive.java
@@ -6,6 +6,27 @@ public class ProblemNaive extends Problem {
 		super(data);
 	}
 
+	public int columnDist(int y, int v) {
+		return Math.abs(y - v);
+	}
+	
+	public int getScoreBalloon(int x, int y) {
+		int score = 0;
+		// (x - u)^2 + (columndist(y, v))^2 < V^2 => score + 1
+		for(int j = y - this.data.getCoverageRadius(); j <= y + V; j++) {
+			for(int i = x - (this.data.getCoverageRadius() - columnDist(y, j);
+					i <= x + (this.data.getCoverageRadius() - columnDist(y, j)); i++) {
+				if(data.isTarget(i,j))
+					score++;
+			}
+		}
+		return score;
+	}
+	
+	public int nextTurnResult(int x, int y, int z, int altitudeDeviation) {
+		
+		return 0;
+	}
 
 	public void resolve() {
 

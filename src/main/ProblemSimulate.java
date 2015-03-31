5f694c866f82941bd6bba215ac928af99efc0ba2
Pierre Falez <pierre.falez@gmail.com>
2015-03-31 23:52:17 +0200

    Fix simulation + optimization

--- src/main/ProblemSimulate.java
+++ src/main/ProblemSimulate.java
@@ -6,7 +6,9 @@ public class ProblemSimulate extends Problem {
 	
 	private boolean[][][] coverredMap;
 	private int[][][][] dynamicScore;
-	
+	private int[][][] currentScore;
+	private int[][] targetMap;
+	private int[][][] destination;
 	
 	private int progress;
 	private int dynProgress;
@@ -26,8 +28,17 @@ public class ProblemSimulate extends Problem {
 			}
 		}
 		
+		targetMap = new int[data.getnX()][data.getnY()];
+		for(int i=0; i<data.getnX(); i++) {
+			for(int j=0; j<data.getnY(); j++) {
+				targetMap[i][j] = data.getTargetIndex(i, j);
+			}
+		}
+		
 		System.out.println(data.getnX()*data.getnY()*data.getnZ()*data.getNbTurn());
 		dynamicScore = new int[data.getnX()][data.getnY()][data.getnZ()][data.getNbTurn()];
+		
+		currentScore = new int[data.getnX()][data.getnY()][data.getNbTurn()];
 		
 		for(int currentBalloon=0; currentBalloon<data.getNbBalloon(); currentBalloon++) {
 			//reset dynamic arrays
@@ -41,7 +52,13 @@ public class ProblemSimulate extends Problem {
 				}
 			}
 			
-			buildBestPath(0);
+			System.out.println("compute score ...");
+			
+			computeScore();
+			
+			System.out.println("compute path ...");
+			
+			//buildBestPath(0);
 			
 			System.out.println("Launch simulatin for balloon "+(currentBalloon+1)+"/"+data.getNbBalloon());
 			
@@ -50,6 +67,8 @@ public class ProblemSimulate extends Problem {
 			
 			List<Integer> path = new ArrayList<Integer>();
 			retrieveBestPath(new Coord3(data.getStartBalloon().x, data.getStartBalloon().y, -1), 0, path);
+			
+			System.out.println("path length : "+path.size());
 			
 			for(int i=0; i<data.getNbTurn(); i++) {
 				this.move[i][currentBalloon] = i < path.size() ? path.get(i) : 0;
@@ -64,7 +83,7 @@ public class ProblemSimulate extends Problem {
 			for(int i=0; i<data.getnX(); i++) {
 				for(int j=0; j<data.getnY(); j++) {
 					for(int k=0; k<data.getnZ(); k++) {
-						dynamicScore[i][j][k][turn] = computeScore(new Coord3(i, j, k), turn);
+						dynamicScore[i][j][k][turn] = currentScore[i][j][turn];
 					}
 				}
 			}	
@@ -104,71 +123,38 @@ public class ProblemSimulate extends Problem {
 						else {
 							score = score3;			
 						}
-						dynamicScore[i][j][k][turn] = computeScore(coordinate, turn)+score;
+						dynamicScore[i][j][k][turn] = currentScore[i][j][turn]+score;
 					}
 				}
 			}
 		}
 		
 
-		System.out.println("best path build for turn "+turn);
+		//System.out.println("[time="+((float)(System.nanoTime()-beg)/1000000.0f)+"ms]best path build for turn "+turn);
 
 			
 	}
 	
 	public int findBestPath(Coord3 coordinate, int turn) {
-		progress++;
-		if(progress % 100000 == 0) {
-			System.out.println("[progress] "+progress+" (dyn_uses="+dynUses+", dyn_progress="+dynProgress+" ["+(((float)dynProgress/(float)(data.getnX()*data.getnY()*data.getnZ()*data.getNbTurn()))*100.0)+"%])");
-			dynUses = 0;
-		}
 		
 		if(turn >= data.getNbTurn())
 			return 0;
 		
 		if(coordinate.z != -1 && dynamicScore[coordinate.x][coordinate.y][coordinate.z][turn] != -1) {
-			dynUses++;
 			return dynamicScore[coordinate.x][coordinate.y][coordinate.z][turn];
 		}
 
 		
 
 		Coord3 c1 = move(coordinate, 0);
-		int score1 = 0;
+		int score1 = c1 != null ? findBestPath(c1,  turn+1) : 0;
 		
-		if(c1 != null) {
-			if(coordinate.z != -1 && turn+1 < data.getNbTurn() && dynamicScore[c1.x][c1.y][c1.z][turn+1] != -1) {
-				dynUses++;
-				score1 = dynamicScore[c1.x][c1.y][c1.z][turn+1];
-				progress++;
-			}
-			else
-				score1 = findBestPath(c1,  turn+1);
-		}
-			
 		Coord3 c2 = move(coordinate, -1);
-		int score2 = 0;
-		if(c2 != null) {
-			if(coordinate.z != -1 && turn+1 < data.getNbTurn() && dynamicScore[c2.x][c2.y][c2.z][turn+1] != -1) {
-				dynUses++;
-				score2 = dynamicScore[c2.x][c2.y][c2.z][turn+1];
-				progress++;
-			}
-			else
-				score2 = findBestPath(c2,  turn+1);
-		}
+		int score2 = c2 != null ? findBestPath(c2,  turn+1) : 0;
 		
 		Coord3 c3 = move(coordinate, 1);
-		int score3 = 0;
-		if(c3 != null) {
-			if(coordinate.z != -1 && turn+1 < data.getNbTurn() && dynamicScore[c3.x][c3.y][c3.z][turn+1] != -1) {
-				dynUses++;
-				score3 = dynamicScore[c3.x][c3.y][c3.z][turn+1];
-				progress++;
-			}
-			else
-				score3 = findBestPath(c3,  turn+1);
-		}
+		int score3 = c3 != null ? findBestPath(c3,  turn+1) : 0;
+
 		
 		int score = 0;
 		if(score1 > score2 && score1 > score3) {
@@ -181,9 +167,16 @@ public class ProblemSimulate extends Problem {
 			score = score3;			
 		}
 		
-		dynProgress++;
-		dynamicScore[coordinate.x][coordinate.y][coordinate.z][turn] = score+computeScore(coordinate, turn);
-		return dynamicScore[coordinate.x][coordinate.y][coordinate.z][turn];
+		if(coordinate.z != -1) {
+			dynamicScore[coordinate.x][coordinate.y][coordinate.z][turn] = score+currentScore[coordinate.x][coordinate.y][turn];
+			if(turn < 2)
+				System.out.println(coordinate.x+" "+coordinate.y+" "+coordinate.z+" "+turn);
+			return dynamicScore[coordinate.x][coordinate.y][coordinate.z][turn];
+		}
+		else {
+			System.out.println(score+" "+coordinate.x+" "+coordinate.y+" "+turn);
+			return score+currentScore[coordinate.x][coordinate.y][turn];
+		}
 	}
 	
 	public Coord3 move(Coord3 coordinate, int altitudeChange) {
@@ -205,23 +198,32 @@ public class ProblemSimulate extends Problem {
 		return new Coord3(x, y, z);
 	}
 	
-	public int computeScore(Coord3 coordinate, int turn) {
-		int score = 0;
+	public void computeScore() {
 		
-		for(int x = -data.getCoverageRadius(); x < data.getCoverageRadius(); x++) {
-			int cX = oRingCompute(coordinate.x+x);
-			for(int y = -data.getCoverageRadius(); y < data.getCoverageRadius(); y++) {
-				int cY = coordinate.y+y;
-				if(y >= 0 && y < data.getnY()) {
-					if(x*x+y*y <= data.getCoverageRadius()*data.getCoverageRadius()) {
-						if(data.getTargetIndex(cX, cY) != -1 && !coverredMap[cX][cY][turn])
-							score++;
+	
+		for(int k=0; k<data.getNbTurn(); k++) {
+			for(int i=0; i<data.getnX(); i++) {
+				for(int j=0; j<data.getnY(); j++) {
+			
+					int score = 0;
+					for(int x = -data.getCoverageRadius(); x < data.getCoverageRadius(); x++) {
+						int cX = oRingCompute(i+x);
+						for(int y = -data.getCoverageRadius(); y < data.getCoverageRadius(); y++) {
+							int cY = j+y;
+							if(cY >= 0 && cY < data.getnY()) {
+								if(x*x+y*y <= data.getCoverageRadius()*data.getCoverageRadius()) {
+									if(targetMap[cX][cY] != -1 && !coverredMap[cX][cY][k])
+										score++;
+								}
+							}
+						}
 					}
+					
+					currentScore[i][j][k] = score;
+					
 				}
 			}
 		}
-		
-		return score;
 	}
 	
 	public void setCovered(Coord3 coordinate, int turn) {
@@ -251,18 +253,17 @@ public class ProblemSimulate extends Problem {
 	}
 	
 	public void retrieveBestPath(Coord3 coordinate, int turn, List<Integer> list) {
-		if(turn >= data.getNbTurn())
+		if(turn >= data.getNbTurn()-1)
 			return;
 		
 		Coord3 c1 = move(coordinate, 0);
-		int score1 = c1 != null ? dynamicScore[c1.x][c1.y][c1.z][turn] : -1;
+		int score1 = c1 != null ? dynamicScore[c1.x][c1.y][c1.z][turn+1] : -1;
 		
 		Coord3 c2 = move(coordinate, -1);
-		int score2 = c2 != null ? dynamicScore[c2.x][c2.y][c2.z][turn] : -1;
+		int score2 = c2 != null ? dynamicScore[c2.x][c2.y][c2.z][turn+1] : -1;
 		
 		Coord3 c3 = move(coordinate, 1);
-		int score3 = c3 != null ? dynamicScore[c3.x][c3.y][c3.z][turn] : -1;
-		
+		int score3 = c3 != null ? dynamicScore[c3.x][c3.y][c3.z][turn+1] : -1;
 
 		if(score1 > score2 && score1 > score3) {
 			list.add(new Integer(0));
@@ -274,11 +275,14 @@ public class ProblemSimulate extends Problem {
 			setCovered(c2, turn);
 			retrieveBestPath(c2, turn+1, list);
 		}
-		else {
+		else if(c3 != null) {
 			list.add(new Integer(1));
 			setCovered(c3, turn);
 			retrieveBestPath(c3, turn+1, list);		
 		}
+		else
+			return;
+
 	}
 
 }
